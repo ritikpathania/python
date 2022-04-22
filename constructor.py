ec5137a66822766e0c61c9de08f04ed4d0b474a8
Ritik Pathania <76494492+ritikpathania@users.noreply.github.com>
2022-04-22 11:05:20 +0530

    Create constructor.py

---
+++ constructor.py
@@ -0,0 +1,10 @@
+class players:
+    def __init__(self):
+        print("Automatically Called")
+    def score(self, playerscore):
+        print(playerscore)
+p1 = players()
+p1.score(33)
+
+p2 = players()
+p2.score(44)
